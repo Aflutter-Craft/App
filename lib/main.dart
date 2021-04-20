e2306ee40a4ef3d65fde4b3a54d2aa365089eedb
blacksuan19 <abubakaryagob@gmail.com>
2021-04-21 03:12:48 +0800

    desktop: set window title and  min size

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:aflutter_craft/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
@@ -14,12 +15,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // check if we are on mobile
-    bool isMobile;
-    try {
-      isMobile = Platform.isAndroid || Platform.isIOS;
-    } catch (e) {
-      isMobile = false;
+    bool isMobile = Platform.isAndroid || Platform.isIOS;
+
+    // set desktop window properties
+    if (!isMobile) {
+      setWindowTitle('Aflutter Craft');
+      setWindowMinSize(Size(1280, 720));
+      setWindowMaxSize(Size.infinite);
     }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Aflutter Craft',
