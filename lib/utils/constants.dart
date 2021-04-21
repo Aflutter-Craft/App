a59786e50254073fc5d9c9ae11f6ac2f03ecc348
blacksuan19 <i@blacksuan19.me>
2021-04-22 04:15:40 +0800

    desktop: about: fix inconsistent text padding
    also remove orange square from network asset

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -7,8 +7,7 @@ A platform for style transfer with neural networks
 // !! is used as a delimeter to split the questions and answers
 const String QUES = '''
 How does it work?!!
-What is the tech stack?
-''';
+What is the tech stack?''';
 
 const String ANS = '''
 The backbone of the application is a Convolutional Neural Network(CNN) with a very complex network architecutre containting a total of 3 models:
@@ -18,13 +17,10 @@ The backbone of the application is a Convolutional Neural Network(CNN) with a ve
 
 The image features are first extracterd using VGG's layers from relu_1_1 until relu5_1 and then passed to the SA module which calculates feature importance for both style and content, after that the content features are multiplied by a value between 0-1 called alpha representing the amount of content the final image will have compared to the original content, after that the final image features go through the decoder which results in the final image.
 
-Figure below shows the model architecture, attention module architecture and identity loss network.
-!!
-when it comes to the tech stack there are 3 main components to the application, the trained model, an API and this application. the model is of course implemented using PyTorch, and the API is a simple general purpose FastAPI application.
+Figure below shows the model architecture, attention module architecture and identity loss network.!!when it comes to the tech stack there are 3 main components to the application, the trained model, an API and this application. the model is of course implemented using PyTorch, and the API is a simple general purpose FastAPI application.
 
 - Model -> python, Pytorch
 - API -> python, FastAPI
 - Application -> Flutter
 
-Figure below shows the overall process of styling an image.
-''';
+Figure below shows the overall process of styling an image.''';
