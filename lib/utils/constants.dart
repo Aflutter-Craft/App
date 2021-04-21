7d763318f426dec99cd458c811043a9e94bd8930
blacksuan19 <i@blacksuan19.me>
2021-04-22 03:55:15 +0800

    utils: split others to appropriate files
    i finally know where these pieces go

---
+++ lib/utils/constants.dart
@@ -0,0 +1,30 @@
+const String BUCKET_PREFIX = "https://aflutter-craft.s3.amazonaws.com/styles/";
+
+const String APP_DISC = '''\n
+A platform for style transfer with neural networks
+''';
+
+// !! is used as a delimeter to split the questions and answers
+const String QUES = '''
+How does it work?!!
+What is the tech stack?
+''';
+
+const String ANS = '''
+The backbone of the application is a Convolutional Neural Network(CNN) with a very complex network architecutre containting a total of 3 models:
+- Pretrained VGG-19: feature extractor, extracts feature maps from images.
+- Style Attentional Network: An attention module (transfomer) that gives certain parts of an image different priorities based on some calculations.
+- Decoder: a network that transforms the results from SAModule to the final output image.
+
+The image features are first extracterd using VGG's layers from relu_1_1 until relu5_1 and then passed to the SA module which calculates feature importance for both style and content, after that the content features are multiplied by a value between 0-1 called alpha representing the amount of content the final image will have compared to the original content, after that the final image features go through the decoder which results in the final image.
+
+Figure below shows the model architecture, attention module architecture and identity loss network.
+!!
+when it comes to the tech stack there are 3 main components to the application, the trained model, an API and this application. the model is of course implemented using PyTorch, and the API is a simple general purpose FastAPI application.
+
+- Model -> python, Pytorch
+- API -> python, FastAPI
+- Application -> Flutter
+
+Figure below shows the overall process of styling an image.
+''';
