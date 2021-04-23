2f9c75486fe9edf9d8021766b64f6ec28f9e808f
blacksuan19 <i@blacksuan19.me>
2021-04-23 10:08:56 +0800

    mobile: initial home page

--- lib/screens/mobile/home.dart
+++ lib/screens/mobile/home.dart
@@ -1,13 +1,57 @@
+import 'package:aflutter_craft/utils/utils.dart';
+import 'package:aflutter_craft/widgets/widgets.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MobileHome extends StatefulWidget {
+class MobileHome extends ConsumerWidget {
   @override
-  _MobileHomeState createState() => _MobileHomeState();
-}
-
-class _MobileHomeState extends State<MobileHome> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, ScopedReader watch) {
+    var content = watch(contentProvider);
+    var style = watch(styleProvider);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.backgroundCol,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          "Aflutter Craft",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 20,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ImageContainer(
+                  image: content,
+                  ratio: 0.4,
+                ),
+                SizedBox(height: 30),
+                ImageContainer(
+                  image: style,
+                  ratio: 0.4,
+                ),
+                SizedBox(height: 20),
+                StyledButton(
+                  btnLabel: "Apply Style",
+                  icon: Icons.check_circle,
+                  onPressed: () => {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      backgroundColor: AppColors.backgroundCol,
+    );
   }
 }
