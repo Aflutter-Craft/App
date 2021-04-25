8efe8bc9322eb3511e60a1fd8b0c198999f05df7
blacksuan19 <abubakaryagob@gmail.com>
2021-04-25 09:01:55 +0800

    providers: use a  provider for shared preferences

--- lib/utils/providers.dart
+++ lib/utils/providers.dart
@@ -2,6 +2,7 @@ import 'package:aflutter_craft/models/models.dart';
 import 'package:aflutter_craft/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:riverpod/riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final contentProvider = StateNotifierProvider(
   (ref) => AppImage(
@@ -30,3 +31,8 @@ final fileProvider = FutureProvider.autoDispose.family((ref, category) async {
 final styleImagesProviders = Categories.values
     .map((category) => fileProvider(category.toShortString()))
     .toList();
+
+// proivder to save some app state permanently to device
+final sharedPrefsProvider = FutureProvider(
+  (ref) async => await SharedPreferences.getInstance(),
+);
