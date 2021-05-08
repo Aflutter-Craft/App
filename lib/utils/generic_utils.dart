ac7502d210a4836abe8c3ae82f51dc0b00bb68f5
blacksuan19 <i@blacksuan19.me>
2021-05-09 01:34:38 +0800

    utils: implement initial API request
    yes am passing watch around
    
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- lib/utils/generic_utils.dart
+++ lib/utils/generic_utils.dart
@@ -1,5 +1,7 @@
 // shows a snackbar with passed text
+
 import 'package:aflutter_craft/utils/utils.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 showToast({context, text}) {
@@ -27,4 +29,44 @@ showToast({context, text}) {
       content: Text(text),
     ),
   );
+}
+
+// the main event of this app
+styleTransfer(watch) async {
+  final content = watch(contentProvider);
+  final style = watch(styleProvider);
+  final alpha = watch(alphaProvider);
+
+  // get content image path
+  final contentPath = content.file.path;
+  // get style image path depending on local or not
+  final stylePath = style is FileImage
+      ? style.file.path
+      : style.url.split('/').skip(4).take(5).join("/");
+
+  // the API URL
+  final url = Uri.parse("");
+
+  var formData = FormData.fromMap({
+    'alpha': alpha / 100, // scale back alpha value
+    'content_img': await MultipartFile.fromFile(contentPath),
+  });
+
+  // check if the style is local file
+  // in that case add it to request files
+  // otherwise pass it as style_path
+  style is FileImage
+      ? formData.files.add(
+          MapEntry(
+            'style_img',
+            await MultipartFile.fromFile(stylePath),
+          ),
+        )
+      : formData.fields.add(
+          MapEntry('style_path', stylePath),
+        );
+
+  var response = await Dio().postUri(url, data: formData);
+
+  print(response);
 }
