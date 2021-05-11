52b93c4383860ae8caad4e4437a95983c2c1ed96
blacksuan19 <i@blacksuan19.me>
2021-05-11 23:09:32 +0800

    results: fix layout on small screens
    my DPI is 570

--- lib/screens/mobile/result.dart
+++ lib/screens/mobile/result.dart
@@ -17,99 +17,104 @@ class ResultsView extends ConsumerWidget {
 
     return Scaffold(
       appBar: mobileAppBar(context: context, label: "Stylized Image"),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                GestureDetector(
-                  onLongPress: () async => showImage(
-                    context: context,
-                    image: content,
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onLongPress: () async => showImage(
+                      context: context,
+                      image: content,
+                    ),
+                    child: GenericContainer(
+                      image: content,
+                      ratio: 0.2,
+                    ),
                   ),
-                  child: GenericContainer(
-                    image: content,
-                    ratio: 0.2,
+                  GestureDetector(
+                    onLongPress: () async => showImage(
+                      context: context,
+                      image: style,
+                    ),
+                    child: GenericContainer(
+                      image: style,
+                      ratio: 0.2,
+                    ),
                   ),
-                ),
-                GestureDetector(
-                  onLongPress: () async => showImage(
-                    context: context,
-                    image: style,
-                  ),
-                  child: GenericContainer(
-                    image: style,
-                    ratio: 0.2,
-                  ),
-                ),
-              ],
-            ),
-            GestureDetector(
-              onLongPress: () async => showImage(
-                context: context,
-                image: result,
+                ],
               ),
-              child: GenericContainer(image: result),
-            ),
-            StyledSlider(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                StyledButton(
-                  btnLabel: "Save",
-                  icon: Icons.save_alt,
-                  // only enable if style transfer has compeleted
-                  onPressed: result is MemoryImage
-                      ? () async {
-                          // save the image to user gallery
-                          await ImageGallerySaver.saveImage(
-                            result.bytes,
-                            name:
-                                "style_transfer_result_${DateTime.now().millisecondsSinceEpoch}.png",
-                          );
+              SizedBox(height: 20),
+              GestureDetector(
+                onLongPress: () async => showImage(
+                  context: context,
+                  image: result,
+                ),
+                child: GenericContainer(image: result),
+              ),
+              SizedBox(height: 20),
+              StyledSlider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  StyledButton(
+                    btnLabel: "Save",
+                    icon: Icons.save_alt,
+                    // only enable if style transfer has compeleted
+                    onPressed: result is MemoryImage
+                        ? () async {
+                            // save the image to user gallery
+                            await ImageGallerySaver.saveImage(
+                              result.bytes,
+                              name:
+                                  "style_transfer_result_${DateTime.now().millisecondsSinceEpoch}.png",
+                            );
 
-                          // show a snackbar
-                          showToast(
-                            context: context,
-                            text: "Image saved to Gallery!",
-                          );
-                        }
-                      : null,
-                ),
-                StyledButton(
-                  btnLabel: "Re-Apply Style",
-                  icon: Icons.check_circle,
-                  // no need to check what the images are because if the user
-                  // made it to this screen then they are already valid
-                  onPressed: () async => await performTransfer(watch),
-                ),
-                StyledButton(
-                  btnLabel: "Share",
-                  icon: Icons.share,
-                  // only enable if style transfer has compeleted
-                  onPressed: result is MemoryImage
-                      ? () async {
-                          // save image to device cache before sharing
-                          final path = await getTemporaryDirectory();
-                          final file = await File(
-                            path.path + "result.png",
-                          ).create();
-                          await file.writeAsBytes(result.bytes);
+                            // show a snackbar
+                            showToast(
+                              context: context,
+                              text: "Image saved to Gallery!",
+                            );
+                          }
+                        : null,
+                  ),
+                  StyledButton(
+                    btnLabel: "Apply Style",
+                    icon: Icons.check_circle,
+                    // no need to check what the images are because if the user
+                    // made it to this screen then they are already valid
+                    onPressed: () async => await performTransfer(watch),
+                  ),
+                  StyledButton(
+                    btnLabel: "Share",
+                    icon: Icons.share,
+                    // only enable if style transfer has compeleted
+                    onPressed: result is MemoryImage
+                        ? () async {
+                            // save image to device cache before sharing
+                            final path = await getTemporaryDirectory();
+                            final file = await File(
+                              path.path + "result.png",
+                            ).create();
+                            await file.writeAsBytes(result.bytes);
 
-                          // share it using default system share options
-                          SocialShare.shareOptions(
-                            "checkout this Image i made with an App called Aflutter Craft!",
-                            imagePath: file.path,
-                          );
-                        }
-                      : null,
-                ),
-              ],
-            )
-          ],
+                            // share it using default system share options
+                            SocialShare.shareOptions(
+                              "checkout this Image i made with an App called Aflutter Craft!",
+                              imagePath: file.path,
+                            );
+                          }
+                        : null,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
